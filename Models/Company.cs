74f2efdc01106f3b7065170335ad9aa44241774f
ferferroro <romelfernando09@gmail.com>
2021-07-06 23:23:16 +0800

    add sample validation

--- Models/Company.cs
+++ Models/Company.cs
@@ -1,5 +1,9 @@
+using System.ComponentModel.DataAnnotations;
 public class Company
 {
     public long Id { get; set; }
+    
+    [Required(ErrorMessage = "This field is required")]
+    // [StringLength(8, ErrorMessage = "Name length can't be more than 8.")]
     public string Name { get; set; }
 }
